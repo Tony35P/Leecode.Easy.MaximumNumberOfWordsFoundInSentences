65ff0fa76ee6460d5aee96d40728d159d0b12af5
Tony <tonytsai27@gmail.com>
2022-12-04 18:41:28 +0800

    add question, solution, unit test

---
+++ Leecode.Easy.MaximumNumberOfWordsFoundInSentences/StringHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Easy.MaximumNumberOfWordsFoundInSentences
+{
+    public class StringHelper
+    {
+        public int MostWordsFound_Foreach(string[] sentences)
+        {
+            var resultArray = new List<int>();
+
+            foreach (var item in sentences)
+            {
+                string[] wordArray = item.Split(' ');
+                int count = wordArray.Length;
+
+                resultArray.Add(count);
+            }
+
+            return resultArray.Max();
+        }
+    }
+}
---
+++ StringHelperTest/MostWordsFoundTest.cs
@@ -0,0 +1,23 @@
+﻿
+
+using NUnit.Framework;
+using System;
+using Leecode.Easy.MaximumNumberOfWordsFoundInSentences;
+
+namespace StringHelperTest
+{
+    internal class MostWordsFoundTest
+    {
+        [TestCase(new String[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }, 6)]
+        [TestCase(new String[] { "please wait", "continue to fight", "continue to win" }, 3)]
+        [TestCase(new String[] { "測試用", "測 試 用", "測 試" }, 3)]
+        public void MostWordsFound_Foreach_ReturnExpected(string[] sentences, int expected)
+        {
+            var helper = new StringHelper();
+
+            var actual = helper.MostWordsFound_Foreach(sentences);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
